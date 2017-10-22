5cfe2736347eb7a3d83ff66c72c81c0a99caf170
jelenanesic <jelenanesicm@gmail.com>
2017-10-22 16:50:02 +0200

    Footer at bottom of page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './footer.scss';
 
 const Footer = () => {
    return(
-      <footer className="Footer row">
-         <div className="col-md-12">
+      <footer className="Footer">
+         <div>
             <p>&copy; <a href="https://www.linkedin.com/in/jelenanesicm/">Jelena</a>, 2017</p>
          </div>
       </footer>
